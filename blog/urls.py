8d61b02d4059cd3dafa26914073de5d18a7e671d
sheffey <gxf121024@outlook.com>
2023-05-01 02:39:26 +0800

    polish: format all docs

--- blog/urls.py
+++ blog/urls.py
@@ -1,24 +1,13 @@
-"""
-blog URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('userprofile/', include(('userprofile.urls', 'userprofile'), namespace='userprofile')),
-    path('article/', include(('article.urls', 'article'), namespace='areicle')),
-    path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archive'),
+    path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
+    # path('search/', views.search, name='search'),
 ]
